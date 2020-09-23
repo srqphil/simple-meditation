387575c0ff9254c2d640f809b8104f30b677bb8e
phillippena <ppena941@gmail.com>
2020-09-23 16:36:12 -0400

    added js file and html elements

---
+++ simplemeditation.js
@@ -0,0 +1,12 @@
+//This function will begin meditation session after the user has selected a valid time
+function $() {document.getElementById();}
+
+function begin () {
+    if ($(time) != "select time") {
+        $("welcome").style.display = "none"
+    }
+
+    else {
+        $(error).innerHTML = "please select a valid time";
+    }
+}
